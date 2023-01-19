00473b53395e480cbd00f1ac91db8ef1e33ff7fa
Vitor Ryan <vitor.ryan.lara.silva@gmail.com>
2023-01-19 16:59:25 -0300

    initial commit

---
+++ carros.js
@@ -0,0 +1,74 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'Ferrari 458 italia',
+        img: 'images/ferrari458.jpg',
+        price: [1000000.00, 1000000.00, 1000000.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: 'Cor vermelha, interna caramelo, atração traseira, roda cinza'
+      },
+      {
+        id: 2,
+        name: 'Ferrari 499',
+        img: 'images/ferrari2.webp',
+        price: [1100000.00, 1100000.00, 1100000.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: 'Cor vermelha, interna preta, atração traseira, roda preta'
+      },
+      {
+        id: 3,
+        name: 'Ferrari 296 GTB',
+        img: 'images/296gtb.jpg',
+        price: [1300000.00, 1300000.00, 1300000.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: 'Cor vermelha, interna caramelo, atração traseira, roda cinza'
+      },
+      {
+        id: 4,
+        name: 'Ferrari 812',
+        img: 'images/812.jpg',
+        price: [1800000.00, 1800000.00, 1800000.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: 'Cor cinza, interna preta, atração traseira, roda cinza'
+      },
+      {
+        id: 5,
+        name: 'Ferrari F8 Spider',
+        img: 'images/f8spider.jpg',
+        price: [2000000.00, 2000000.00, 2000000.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: 'Cor amarela, interna preta, atração traseira, roda preta'
+      },
+      {
+        id: 6,
+        name: 'Ferrari SF90',
+        img: 'images/SF90.jpg',
+        price: [2100000.00, 2100000.00, 2100000.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: 'Cor preta, interna preta, atração traseira, roda preta'
+      }
+];
